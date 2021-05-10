f12d8942f4b8239ad8d127a5d044770de8e99c10
zaynejamilQA <zjamil@qa.com>
2021-05-10 15:11:05 +0100

    ZJ: Code written for each test, some errors require fixing.

--- BackEnd/MmaProject/src/test/java/com/qa/mma/service/MmaServiceUnitTest.java
+++ BackEnd/MmaProject/src/test/java/com/qa/mma/service/MmaServiceUnitTest.java
@@ -1,6 +1,11 @@
 package com.qa.mma.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -16,20 +21,70 @@ public class MmaServiceUnitTest {
 
 	@Autowired
 	private MmaService service;
-	
+
 	@MockBean
 	private MmaRepo repo;
-	
+
 	@Test
 	void testCreate() {
-		//GIVEN
-		Mma conor = new Mma("Conor McGregor", 32, 23, 5, 0, 0);
-		Mma savedConor = new Mma(1L, "Conor McGregor", 32, 23, 5, 0, 0);
-		//WHEN
-		Mockito.when(this.repo.save(conor)).thenReturn(savedConor);
-		//THEN
-		assertThat(this.service.create(conor)).isEqualTo(savedConor);
-		//CHECKS
-		Mockito.verify(this.repo, Mockito.times(1)).save(conor);
+		// GIVEN
+		final Mma input = new Mma("Conor McGregor", 32, 23, 5, 0, 0);
+		final Mma expected = new Mma(1L, "Conor McGregor", 32, 23, 5, 0, 0);
+		// WHEN
+		Mockito.when(this.repo.save(input)).thenReturn(expected);
+		// THEN
+		assertThat(this.service.create(input)).isEqualTo(expected);
+		// CHECKS
+		Mockito.verify(this.repo, Mockito.times(1)).save(input);
+	}
+
+	@Test
+	void testGetAll() {
+		final List<Mma> expected = new ArrayList<>();
+		expected.add(new Mma(1L, "Conor McGregor", 32, 23, 5, 0, 0));
+		Mockito.when(this.repo.findAll()).thenReturn(expected);
+		assertThat(this.service.getAll()).isEqualTo(expected);
+		Mockito.verify(this.repo, Mockito.times(1)).findAll();
+	}
+
+	@Test
+	void testGetById() {
+		final long ID = 1L;
+		final Optional<Mma> Optionalexpected = Optional.of(new Mma(1L, "Conor McGregor", 32, 23, 5, 0, 0));
+		final Mma expected = new Mma(1L, "Conor McGregor", 32, 23, 5, 0, 0);
+		Mockito.when(this.repo.findById(ID)).thenReturn(Optionalexpected);
+		assertThat(this.service.getById(ID)).isEqualTo(expected);
+		Mockito.verify(this.repo, Mockito.times(1)).findById(ID);
+	}
+
+	@Test
+	void testGetByName() {
+		final String NAME = "Conor McGregor";
+		final Mma expected = new Mma(1L, "Conor McGregor", 32, 23, 5, 0, 0);
+		Mockito.when(this.repo.findByName(NAME)).thenReturn(expected);
+		assertThat(this.service.getFighterByName(NAME)).isEqualTo(expected);
+		Mockito.verify(this.repo, Mockito.times(1)).findByName(NAME);
+	}
+
+	@Test
+	void testRemove() {
+		final long ID = 1L;
+		Mockito.when(this.repo.existsById(ID)).thenReturn(false);
+		assertThat(this.service.remove(ID)).isEqualTo(true);
+		Mockito.verify(this.repo, Mockito.times(1)).deleteById(ID);
+		Mockito.verify(this.repo, Mockito.times(1)).existsById(ID);
+	}
+
+	@Test
+	void testUpdateFighter() {
+		final long ID = 1L;
+		final Mma updated = new Mma(1L, "'The Notourious' Conor McGregor", 33, 26, 6, 1, 1);
+		final Optional<Mma> Optionalexpected = Optional.of(new Mma(1L, "Conor McGregor", 32, 23, 5, 0, 0));
+		final Mma expected = new Mma(1L, "Conor McGregor", 32, 23, 5, 0, 0);
+		Mockito.when(this.repo.findById(ID)).thenReturn(Optionalexpected);
+		Mockito.when(this.repo.save(updated)).thenReturn(updated);
+		assertThat(this.service.updateFighter(ID, updated)).isEqualTo(updated);
+		Mockito.verify(this.repo, Mockito.times(1)).findById(ID);
+		Mockito.verify(this.repo, Mockito.times(1)).save(updated);
 	}
 }
